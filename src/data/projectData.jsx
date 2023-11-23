920c6991516da983d64d6102ba7ba8bdff197a4e
joshua <joshua@joshuas-Air.lan>
2023-11-23 11:47:32 -0600

    fix

--- src/data/projectData.jsx
+++ src/data/projectData.jsx
@@ -1,15 +1,16 @@
-import gitHubImage from '../assets/projectCardImages/github.png'
+import github from '../assets/projectCardImages/github.png'
 import trans from '../assets/projectCardImages/trans.png'
+import market from '../assets/projectCardImages/market.png'
 
 export const projectData = [
-    // {
-    //     id: 1,
-    //     image: gitHubImage,
-    //     categories: ['B2B', 'B2C', 'Web'],
-    //     title: 'Github - Streamlining Access To Shared Cloud Storage for Non-Dropbox users',
-    //     desc: 'I proactively identified friction in the signup journey for non-Dropbox users, proposed redesigning it, and helped prioritize it on the product roadmap. Released in Sep 21.',
-    //     link: 'hello'
-    // },
+    {
+        id: 1,
+        image: github,
+        categories: ['B2B', 'B2C', 'Web', 'Branding'],
+        title: '',
+        desc: '',
+        link: '/github'
+    },
     {
         id: 2,
         image: trans,
@@ -18,14 +19,14 @@ export const projectData = [
         desc: 'I proactively identified friction in load and driver discovery, proposed redesigning it, and helped prioritize it on the product roadmap.',
         link: '/transportpro'
     },
-    // {
-    //     // id: 3,
-    //     // image: 'https://assets-global.website-files.com/63995be510337137d26a0abc/6525884e444069328ee14e65_Dropbox%20signup%20left%20(15)-p-800.jpg',
-    //     // categories: ['B2C', 'Web', 'Branding'],
-    //     // title: 'Verizon - Streamlining Access To Shared Cloud Storage for Non-Dropbox users',
-    //     // desc: 'I proactively identified friction in the signup journey for non-Dropbox users, proposed redesigning it, and helped prioritize it on the product roadmap. Released in Sep 21.'
-        
-    // },
+    {
+        id: 3,
+        image: market,
+        categories: ['B2B', 'Web'],
+        title: 'Marketdeus - Streamlining the Email Marketing Process for Companies and their customers',
+        desc: 'As the designer and developer for this marketing software, I played a pivotal strategic role, driving initiatives that doubled user retention in our A/B testing',
+        link: '/market'
+    },
     // {
     //     // id: 4,
     //     // image: 'https://assets-global.website-files.com/63995be510337137d26a0abc/652b5aa587d36e6c665eabe4_Dropbox%20signup%20left%20(4)-p-800.jpg',
--- src/pages/Marketdeus.jsx
+++ src/pages/Marketdeus.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import one from '../assets/market/one.webp'
 
 const Market = () => {
     return (
         <>
-            Market
+            <img src={one} />
         </>
     )
 }
