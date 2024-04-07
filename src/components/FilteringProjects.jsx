1504fbafccd61051eac2ce16c030675cd101f19b
joshua <joshua@joshuas-Air-2.lan>
2024-04-07 16:45:43 -0500

    fix cilor buttons in dark mode

--- src/components/FilteringProjects.jsx
+++ src/components/FilteringProjects.jsx
@@ -62,7 +62,7 @@ const FilteringProjects = () => {
               marginRight: 3,
               height: "40px",
               fontSize: 2,
-              backgroundColor: "canvas.default",
+              backgroundColor: "canvas.sublte",
               marginBottom: 3,
               boxShadow: " 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)",
               transition: "all 0.3s cubic-bezier(.25,.8,.25,1)"
@@ -80,7 +80,7 @@ const FilteringProjects = () => {
               marginRight: 3,
               height: "40px",
               fontSize: 2,
-              backgroundColor: "canvas.default",
+              backgroundColor: "canvas.sublte",
               marginBottom: 3,
               boxShadow: " 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)",
               transition: "all 0.3s cubic-bezier(.25,.8,.25,1)"
@@ -98,7 +98,7 @@ const FilteringProjects = () => {
               marginRight: 3,
               height: "40px",
               fontSize: 2,
-              backgroundColor: "canvas.default",
+              backgroundColor: "canvas.sublte",
               marginBottom: 3,
               boxShadow: " 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)",
               transition: "all 0.3s cubic-bezier(.25,.8,.25,1)"
@@ -116,7 +116,7 @@ const FilteringProjects = () => {
               marginRight: 3,
               height: "40px",
               fontSize: 2,
-              backgroundColor: "canvas.default",
+              backgroundColor: "canvas.sublte",
               marginBottom: 3,
               boxShadow: " 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)",
               transition: "all 0.3s cubic-bezier(.25,.8,.25,1)"
@@ -134,7 +134,7 @@ const FilteringProjects = () => {
               marginRight: 3,
               height: "40px",
               fontSize: 2,
-              backgroundColor: "canvas.default",
+              backgroundColor: "canvas.sublte",
               marginBottom: 3,
               boxShadow: " 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)",
               transition: "all 0.3s cubic-bezier(.25,.8,.25,1)"
